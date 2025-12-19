8660f6ff17e1334cefe354a86bbe8555c7cc40dc
BIEHM <BIEHM@192.168.1.20>
2025-12-19 14:29:16 +0100

    --- automatic rebase to remote; old local SHA: 9b40b040be2ab1e2de5872d34de892466a58c3e6 ---

---
+++ src/com/kuka/generated/ioAccess/ProfinetIOGroup.java
@@ -0,0 +1,97 @@
+package com.kuka.generated.ioAccess;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import com.kuka.roboticsAPI.controllerModel.Controller;
+import com.kuka.roboticsAPI.ioModel.AbstractIOGroup;
+import com.kuka.roboticsAPI.ioModel.IOTypes;
+
+/**
+ * Automatically generated class to abstract I/O access to I/O group <b>Profinet</b>.<br>
+ * <i>Please, do not modify!</i>
+ * <p>
+ * <b>I/O group description:</b><br>
+ * ./.
+ */
+@Singleton
+public class ProfinetIOGroup extends AbstractIOGroup
+{
+	/**
+	 * Constructor to create an instance of class 'Profinet'.<br>
+	 * <i>This constructor is automatically generated. Please, do not modify!</i>
+	 *
+	 * @param controller
+	 *            the controller, which has access to the I/O group 'Profinet'
+	 */
+	@Inject
+	public ProfinetIOGroup(Controller controller)
+	{
+		super(controller, "Profinet");
+
+		addInput("In1", IOTypes.BOOLEAN, 1);
+		addDigitalOutput("Out1", IOTypes.BOOLEAN, 1);
+	}
+
+	/**
+	 * Gets the value of the <b>digital input '<i>In1</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital input
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital input 'In1'
+	 */
+	public boolean getIn1()
+	{
+		return getBooleanIOValue("In1", false);
+	}
+
+	/**
+	 * Gets the value of the <b>digital output '<i>Out1</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital output
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @return current value of the digital output 'Out1'
+	 */
+	public boolean getOut1()
+	{
+		return getBooleanIOValue("Out1", true);
+	}
+
+	/**
+	 * Sets the value of the <b>digital output '<i>Out1</i>'</b>.<br>
+	 * <i>This method is automatically generated. Please, do not modify!</i>
+	 * <p>
+	 * <b>I/O direction and type:</b><br>
+	 * digital output
+	 * <p>
+	 * <b>User description of the I/O:</b><br>
+	 * ./.
+	 * <p>
+	 * <b>Range of the I/O value:</b><br>
+	 * [false; true]
+	 *
+	 * @param value
+	 *            the value, which has to be written to the digital output 'Out1'
+	 */
+	public void setOut1(java.lang.Boolean value)
+	{
+		setDigitalOutput("Out1", value);
+	}
+
+}
